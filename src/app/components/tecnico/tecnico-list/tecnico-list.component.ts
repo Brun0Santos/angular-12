5c8817e1b0988594ece2f97542bc0b9f10e94dc2
Bruno Santos <bruno_st17@hotmail.com>
2024-09-02 22:35:31 -0300

    feat: adicionando o componente de tecnico

---
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, ViewChild, AfterViewInit  } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnico';
+
+
+@Component({
+  selector: 'app-tecnico-list',
+  templateUrl: './tecnico-list.component.html',
+  styleUrls: ['./tecnico-list.component.css']
+})
+
+export class TecnicoListComponent implements OnInit, AfterViewInit  {
+
+  ELEMENT_DATA: Array<Tecnico> = [
+    {id: 1, nome:'Bruno', cpf: '050550505050', email: 'teste@gmail.com', senha: '1q2w3e4r', perfis: ['0'], dataCriacao: '31/08/2024'}
+  ]
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+  
+
+}
