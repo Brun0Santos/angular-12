cfabda2d71795d99718d79a4cbae16db1c5e8cbc
Bruno Santos <bruno_st17@hotmail.com>
2024-09-11 22:15:47 -0300

    feat: adicionando o modulo

---
+++ src/app/components/tecnico/tecnico-list/tecnico-list.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TecnicoListRoutingModule } from './tecnico-list-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TecnicoListRoutingModule
+  ]
+})
+export class TecnicoListModule { }
