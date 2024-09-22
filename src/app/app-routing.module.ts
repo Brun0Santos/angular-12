dd322aa345d4e3f4db436f107edb4ae30b17d5ba
Bruno Santos <bruno_st17@hotmail.com>
2024-09-21 22:43:56 -0300

    feat: adicionando o module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
+import { AuthGuard } from './auth/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: NavComponent, children: [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'tecnicos', loadChildren: () => import('./components/tecnico/tecnico-list/tecnico-list.module').then((m) => m.TecnicoListModule)}
+  ]},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
