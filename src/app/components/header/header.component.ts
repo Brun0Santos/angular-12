bf13105c88c80a4b1d5cfdd16cae01443bfac086
Bruno Santos <bruno_st17@hotmail.com>
2024-10-27 22:34:24 -0300

    fix: adicionando o ts

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -10,7 +10,7 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    console.log("Realizando alguns testess")
+    console.log("Realizando alguns testes")
   }
 
 }
