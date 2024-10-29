5b6ccb1b474f157536db8809f4e829408c8199bc
Bruno Santos <bruno_st17@hotmail.com>
2024-10-28 23:02:47 -0300

    fix: ajustando a rota

--- src/app/components/tecnico/tecnico-list/tecnico-list-routing.module.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { TecnicoListComponent } from './tecnico-list.component';
 
 const routes: Routes = [
-  {path: '', component: TecnicoListComponent}
+  { path: '', component: TecnicoListComponent }
 ];
 
 @NgModule({
