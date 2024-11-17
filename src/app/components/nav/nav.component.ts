c8d4b7c1a06e2e0db754afbf811189fd17bb0820
Bruno Santos <bruno_st17@hotmail.com>
2024-11-17 19:42:00 -0300

    fix: ajustes

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -12,7 +12,7 @@ export class NavComponent implements OnInit {
 
   ngOnInit(): void {
     // this.router.navigate(['homes'])
-    localStorage.setItem("token", "wdqdqw413rqwrwe")
+    localStorage.setItem("token", "wdqdqw413rqe")
   }
 
 }
