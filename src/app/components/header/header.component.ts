dc02887a4fb4ffc5f9f27b3d0f96ba1eed13f685
Bruno Santos <bruno_st17@hotmail.com>
2024-12-08 20:39:50 -0300

    fix: testess

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -10,7 +10,7 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    console.log("Realizando alguns testes na services")
+    console.log("Realizando alguns testes na servicessss")
   }
 
 }
